7c1d48cfc4b13af2206bd642884b8137de9e5a46
LCobham <lucascobham@hotmail.com>
2022-11-03 12:22:58 -0700

    Included guard in main.h

--- main.h
+++ main.h
@@ -2,10 +2,16 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 int _putchar (char c);
 int _printf(const char *format, ...);
+
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+
+#include <stdlib.h>
 
 #endif
